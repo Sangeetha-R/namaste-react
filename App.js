feed7e81e38676f5e71c859529c6d1f32e932d64
Sangeetha Ragunath <sangeetha@Sangeethas-MacBook-Air.local>
2023-09-05 21:49:03 +0530

    episode - 01

---
+++ App.js
@@ -0,0 +1,5 @@
+const header = React.createElement("h1", {id: "header"}, [
+    React.createElement("span", {}, "I am sample text")
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
